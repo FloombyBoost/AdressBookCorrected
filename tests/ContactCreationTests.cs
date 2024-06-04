751b42e2d7e798ede88212090007e7e94330a543
Evgenii Emelianov <evgeniy-95@bk.ru>
2024-06-04 19:58:43 +0300

    15 Задание

--- tests/ContactCreationTests.cs
+++ tests/ContactCreationTests.cs
@@ -1,11 +1,16 @@
 ﻿using AdressBook_web_test;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using NUnit.Framework.Legacy;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+using System.IO;
 using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace AdressBook_web_test
 {
@@ -33,7 +38,71 @@ namespace AdressBook_web_test
         }
 
 
+        public static IEnumerable<ContactData> GroupDataFromCsvFile()
+        {
+            List<ContactData> contact = new List<ContactData>();
+            string[] lines = File.ReadAllLines(@"contacts.csv");
+            foreach (string l in lines)
+            {
+                string[] parts = l.Split(',');
+                contact.Add(new ContactData(parts[0], parts[1])
+                {
+                    WorkPhone = parts[2]
+                   
+                });
+            }
 
+            return contact;
+
+        }
+
+
+
+
+        public static IEnumerable<ContactData> ContactDataFromXmlFile()
+        {
+
+            return (List<ContactData>)new XmlSerializer(typeof(List<ContactData>))
+               .Deserialize(new StreamReader(@"contacts.xml"));
+
+
+        }
+
+
+        public static IEnumerable<ContactData> ContactDataFromJsonFile()
+        {
+
+            return JsonConvert.DeserializeObject<List<ContactData>>(File.ReadAllText(@"contacts.json"));
+
+
+        }
+
+        public static IEnumerable<ContactData> ContactDataFromExcelFile()
+        {
+            List<ContactData> contact = new List<ContactData>();
+            Excel.Application app = new Excel.Application();
+            Excel.Workbook wb = app.Workbooks.Open(Path.Combine(Directory.GetCurrentDirectory(), "contacts.xlsx"));
+            Excel.Worksheet sheet = wb.ActiveSheet;
+            Excel.Range range = sheet.UsedRange;
+            for (int i = 1; i <= range.Rows.Count; i++)
+            {
+                contact.Add(new ContactData()
+                {
+                    Address = range.Cells[i, 1].Value,
+                    Email2 = range.Cells[i, 2].Value,
+                    WorkPhone = range.Cells[i, 3].Value
+
+                });
+            }
+            wb.Close();
+            app.Visible = false;
+            app.Quit();
+            return contact;
+
+
+        }
+
+       
         [Test, TestCaseSource("RandomContactDataProvider")]//15 задание
         public void ContactCreationTestDataDriven(ContactData contact)
         {
